acef6da9831e23236f974a08fcc2132a17bd2e98
Dmytro Shulha <dimonchik0218@gmail.com>
2026-07-18 14:27:20 +0300

    fix checllist #6 & #7

--- app/settings.py
+++ app/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&(xp^zt=ex7yn#fvo!4_7%fj80yhk1cso*w4vomr7tnisok*lb"
+SECRET_KEY = (
+    "django-insecure-&(xp^zt=ex7yn#fvo!4_7%fj80yhk1cso*w4vomr7tnisok*lb"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- cinema/serializers.py
+++ cinema/serializers.py
@@ -5,7 +5,7 @@ from cinema.models import Movie
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=True, max_length=255)
-    description = serializers.CharField(required=False)
+    description = serializers.CharField()
     duration = serializers.IntegerField(required=True)
 
     def create(self, validated_data):
@@ -14,8 +14,7 @@ class MovieSerializer(serializers.Serializer):
     def update(self, instance, validated_data):
         instance.title = validated_data.get("title", instance.title)
         instance.description = validated_data.get(
-            "description",
-            instance.description
+            "description", instance.description
         )
         instance.duration = validated_data.get("duration", instance.duration)
         instance.save()
